4843ab8d49b7cf552125552e215f8d7ac5905216
yatvive <215081267+yatvive@users.noreply.github.com>
2026-07-09 12:29:30 -0400

    mon commit comment

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,26 +1,3 @@
-{/*
-import React, { useState, useEffect } from 'react';
-import './ProductList.css'
-import {addItem} from "./CartSlice.jsx";
-import { useDispatch, useSelector } from 'react-redux'; 
-import './ProductList.css';
-import CartItem from './CartItem';
-function ProductList({ onHomeClick }) {
-    const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
-    // 4. Initialisation essentielle du hook dispatch
-    const dispatch = useDispatch(); 
-    const handleAddToCart = (product) => {
-        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
-        }));
-      };
-      */}
-
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; 
 import './ProductList.css';
